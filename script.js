d442f5bf0f88f4817ba81c9fd1315b89fdaa45f6
adhikari-ishan <adhikariishan02@gmail.com>
2026-06-30 16:57:40 +0545

    new commit day 3

--- script.js
+++ script.js
@@ -3,19 +3,39 @@ const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
 
 const tasks = document.querySelectorAll(".task");
+let dragElement = null;
 
-// const task = document.querySelector(".task");
 
 tasks.forEach(task => {
     task.addEventListener("drag", (e)=>{
-       console.log("dragging",e);
+    e.preventDefault();
+    dragElement = task;
     })
 })
 
-progress.addEventListener("dragenter", (e)=>{
-    progress.classList.add("hover-over");
-})
 
-progress.addEventListener("dragleave", (e)=>{
-    progress.classList.remove("hover-over");
-})
+function borderEffect(col){
+    col.addEventListener("dragenter",(e)=>{
+        e.preventDefault();
+        col.classList.add("hover-over");
+    })
+
+    col.addEventListener("dragleave", (e)=>{
+        e.preventDefault();
+        col.classList.remove("hover-over");
+    })
+
+    col.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+    })
+
+    col.addEventListener("drop", (e)=>{
+        e.preventDefault();
+        col.appendChild(dragElement);
+        col.classList.remove("hover-over");
+    })
+}
+
+borderEffect(todo);
+borderEffect(progress);
+borderEffect(done);
