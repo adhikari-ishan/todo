8a2d1cf06f9220c44b2d6f78b38a1d124ff66af4
adhikari-ishan <adhikariishan02@gmail.com>
2026-07-01 14:50:56 +0545

    task can be added and dragable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
@@ -39,3 +40,33 @@ function borderEffect(col){
 borderEffect(todo);
 borderEffect(progress);
 borderEffect(done);
+
+const toggleModalButton = document.getElementById("toggle-modal");
+const modal = document.querySelector(".modal");
+const bgmodal = document.querySelector(".modal .bg");
+const addTaskButton = document.getElementById("add-new-task");
+
+toggleModalButton.addEventListener("click", ()=>{
+    modal.classList.toggle("active");
+})
+
+bgmodal.addEventListener("click", ()=>{
+    modal.classList.remove("active");
+})
+
+addTaskButton.addEventListener("click", ()=>{
+    const taskTitle = document.getElementById("task-title-input").value;
+    const taskDesc = document.getElementById("task-desc-input").value;
+    modal.classList.remove("active");
+    const div = document.createElement("div"); 
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+    div.innerHTML = `<h2>${taskTitle}</h2>
+                    <p> ${taskDesc} </p>
+                    <button id="deletebtn">Delete</button>`;
+    todo.appendChild(div);  
+    div.addEventListener("drag", ()=>{
+        preventDefault();
+        dragElement = task;
+    })             
+})
